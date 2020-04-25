99faada0e415f1ecaf5f2e5336769a7cbd8521b7
dmrapuano1 <dmrapuano1@gmail.com>
2020-04-25 19:38:07 -0400

    cleaned code

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -1,14 +1,14 @@
 import puppeteer from 'puppeteer';
 
-describe('Show/hide an event\'s details', () => {
+describe('Show/hide an event details', () => {
 
   test('an event is collapsed by default', async () => {
     jest.setTimeout(30000);
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
-    await page.goto('http://localhost:3000/meetup_application');
-    await page.waitForSelector('.events');
-    const visibility = await page.$('.events .visibility');
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+    const visibility = await page.$('.event .visibility');
     expect(visibility).toBeNull();
     browser.close();
   });
