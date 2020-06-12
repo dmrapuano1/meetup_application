0eb9facca27722fe2e49b82b97e4c836f6936e87
dmrapuano1 <dmrapuano1@gmail.com>
2020-06-12 13:11:40 -0400

    syntax fixes for warning text

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -11,37 +11,39 @@ class NumberOfEvents extends Component {
     const value = event.target.value;
     this.setState({ eventNumber: value });
 
-    if (value < 0 || value == 0 || !value) {
+    if (value < 1 || !value) {
       this.setState({
-        infoText: 'Value must be at least 1'
+        warningText: 'Value must be at least 1'
       })
       this.props.updateCount(1)
     } else if (value > 250) {
       this.setState({
-        infoText: 'Value must be less than 250'
+        warningText: 'Value must be less than 250'
       })
       this.props.updateCount(250)
     } else {
       this.setState({
-        infoText: ''
+        warningText: ''
       })
       this.props.updateCount(value);
-    }
+    }  
   }
 
   render() {
     
     return (
       <div className="NumberOfEvents">
-        <ErrorAlert text={this.state.infoText} />
-        Show
-        <input 
-          type="number"
-          className="totalEvents"
-          value={this.state.eventNumber}
-          onChange={this.handleInputChanged}
-        />
-        Events
+        <ErrorAlert text={this.state.warningText} />
+        <div>
+          Show
+          <input 
+            type="number"
+            className="totalEvents"
+            value={this.state.eventNumber}
+            onChange={this.handleInputChanged}
+          />
+          Events
+        </div>
       </div>
     );
   }
