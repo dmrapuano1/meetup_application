26924bfaa81ea846484640c65d313361b351bffb
dmrapuano1 <dmrapuano1@gmail.com>
2020-06-15 14:52:38 -0400

    added graph to display number of events per day

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './css/App.css'
+import './css/App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import {getEvents, checkStatus} from './api'
-import {CautionAlert} from './Alert'
+import {getEvents, checkStatus} from './api';
+import {CautionAlert} from './Alert';
+import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
+import moment from 'moment';
 
 class App extends Component {
 
@@ -46,7 +48,31 @@ class App extends Component {
       } else {
         this.setState({ cautionText: '' })
       }
+  }
+
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
     }
+    return count;
+  }
+
+  getData = () => {
+    const next7Days = []; // Create empty array for the next 7 days
+    const currentDate = moment(); // Today
+    // Loop 7 times for next 7 days
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days'); // Add 1 day to current date, currentDate changes
+      const dateString = currentDate.format('YYYY-MM-DD'); // Format the date
+      // Use the countEventsOnADate function to count #events on this date
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count }); // Add this date and number to the list
+    }
+    return next7Days;
+  }
 
   render () {
     console.log(this.state.cautionText)
@@ -55,6 +81,19 @@ class App extends Component {
         <CitySearch updateEvents={this.updateEvents} changeCautionText={offline => this.changeCautionText(offline)}/>
         <NumberOfEvents updateCount={this.updateCount} changeCautionText={offline => this.changeCautionText(offline)}/>
         <CautionAlert text={this.state.cautionText} />
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
       </div>
     );
